7d65598969ffdfa1340d8e8e17fc35c58d02bb0e
katsuki-ino <katsuki_ino@fronteo.com>
2020-12-18 12:17:52 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+import pickle
+import json
+import os
+import glob
+import re
+import base64
+
+import flask
+import pandas as pd
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_cytoscape as cyto
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+
+from cytograph import cytograph
+from edge_generator import make_drug_table
+
+# Load extra layouts
+cyto.load_extra_layouts()
+
+# app initialize
+app = dash.Dash(__name__,
+                # url_base_pathname='/interactive_cascade/',
+                # external_stylesheets=[dbc.themes.DARKLY],
+                # serve_locally=False,
+                prevent_initial_callbacks=True, 
+                # suppress_callback_exceptions=True
+                )
+
+app.layout = html.Div(
+    [
+    cytograph,
+    html.Div([
+        dash_table.DataTable(
+        columns=[{'id': c, 'name': c} for c in ['Drug', 'Phase']],
+        page_size=5,
+        id='drug_table',
+        style_cell_conditional=[
+            {'if': {'column_id': 'Drug'},
+            'width': '70%'},
+            {'if': {'column_id': 'Phase'},
+            'width': '10%'},
+        ],
+        tooltip_delay=0,
+        tooltip_duration=None
+        )
+    ], style={'width':'80%'})
+    ]
+)
+
+
+@app.callback([Output('drug_table', 'data'), Output('drug_table', 'tooltip_data')],
+            [Input('cytoscape-elements-classes', 'tapNodeData')])           
+def update_janbotron_by_tap(target):
+    target = target['id']
+    return make_drug_table(target)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+    # app.run_server()
---
+++ edge_generator.py
@@ -0,0 +1,79 @@
+import pandas as pd 
+import numpy as np
+import pickle
+
+import dash_table
+# phase_df = pd.read_json('phase_data.json')
+with open('data/noVCP/gene_drug_report.pkl', 'rb') as f:
+    phase_df = pickle.load(f)
+phase_df = phase_df.set_index('Gene')
+
+with open('data/noVCP/drug_properties.pkl', 'rb') as f:
+    drug_properties = pickle.load(f)
+
+
+def paging():
+    
+    path = pd.read_table('NGLY1_plot_graph_data_20201214_no_VCP.txt')
+    # phase_df = pd.read_json('phase_data.json')
+
+    # edge処理
+    path = path[path['sources']!='S']
+    path = path[path['targets']!='E']
+    s = list(path['sources'].unique())
+    t = list(path['targets'].unique())
+    starts = [n for n in s if n not in t]
+    ends = [n for n in t if n not in s]
+
+    edges = []
+    for source, target, flow in zip(path['sources'], path['targets'], path['flow']):
+        if source=='S' or target=='E':
+            continue
+        else:
+            edges.append({'data':{'source': source, 'target': target, 'weight':flow}, 'selectable': 'True'})
+
+    # node処理
+    nodes = []
+    for edge in edges:
+        data = edge['data']
+        nodes.append(data['source'])
+        nodes.append(data['target'])
+    
+    nodes = list(set(nodes))
+
+    page_nodes = []
+    for name in nodes:
+        if phase_df.loc[name, 'max_phase']==4:
+            page_nodes.append({'data': {'id': name, 'label': name}, 'classes': 'havedrug'})
+        elif name in starts:
+            page_nodes.append({'data': {'id': name, 'label': name}, 'classes': 'causality'})
+        elif name in ends:
+            page_nodes.append({'data': {'id': name, 'label': name},  'classes': 'responsive'})
+        else:
+            page_nodes.append({'data': {'id': name, 'label': name}})
+            
+    return page_nodes, edges
+
+
+def make_drug_table(target):
+    def make_tooptip(drug):
+        indications = drug_properties[drug_properties['compound_name']==drug]
+        if len(indications)==0:
+            return 'No Drug Indication'
+        else:
+            sent = 'Drug Indications  \n [Target Disease: Clinical Phase]  \n\n\n'
+            
+            for efo, max_phase in zip(indications['efo_term'], indications['max_phase_for_ind']):
+                line = '{}: {}  \n'.format(efo, max_phase)
+                sent += line
+            return sent
+
+    drugs = phase_df.loc[target, 'Drug']
+    phases = phase_df.loc[target, 'Phase']
+    df = pd.DataFrame({'Drug':drugs, 'Phase':phases})
+
+    tooltip = [{
+        c: {'value': make_tooptip(drug), 'type': 'markdown'}
+        for c in ['Drug']
+    } for drug in drugs]
+    return df.to_dict('records'), tooltip
